1d2005ae5983c160802d30c2d1ecf757058bb903
YordanDeVp <yordanyrsm@gmail.com>
2026-05-09 00:44:34 -0500

    modify server,js

--- server.js
+++ server.js
@@ -49,7 +49,6 @@ async function initial() {
 }
 
 app.get("/", (req, res) => {
-  console.log('Este es el Despliege para el Laboratorio de la semana 08');
   res.json({ message: "Welcome to the Node.js JWT Authentication API." });
 });
 
